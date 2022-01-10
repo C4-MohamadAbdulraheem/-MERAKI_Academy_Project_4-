a8ed663481edb56df1eae4474868b5b177d1ba2b
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-01-10 16:01:47 +0200

    create comment schema

--- backend/database/models/commentSchema.js
+++ backend/database/models/commentSchema.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-const comment = new mongoose.Schema({
+const commentSchema = new mongoose.Schema({
     comment:{type:required},
     commenter:{type:mongoose.Types.Schema.ObjectId , ref:"User"}
 })
+module.exports = mongoose.model("Comment",commentSchema)
