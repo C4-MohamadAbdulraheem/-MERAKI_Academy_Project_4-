26f6ba8644a377299abb4ef986b92febb52a096a
MohamadAbdulraheem <mohammad.o.abdulraheem@gmail.com>
2022-01-12 00:48:09 +0200

    create authorization middleware

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+const jwtdecode = require("jwt-decode");
+
+const authorization = (permission) => {
+  permission = permission.toLowerCase();
+  return (req, res, next) => {
+    let token = req.token;
+    token = jwtdecode(token);
+    const mapOverpermissions = token.role.permissions.map((ele) => {
+      return ele.toLowerCase();
+    });
+    if (mapOverpermissions.includes(permission)) {
+      next();
+    } else {
+      res.status(403).json({
+        success: false,
+        message: "Un Authorized",
+      });
+    }
+  };
+};
+
+module.exports = { authorization };
--- backend/routes/commentRouter.js
+++ backend/routes/commentRouter.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createNewComment } = require("../controllers/comments");
 const { authentication } = require("../middleware/authentication");
+const { authorization } = require("../middleware/authorization");
 //create commentRouter
 const commentRouter = express.Router();
 
 //end points for commentRouter
 
 // end point to create new comment
-commentRouter.post("/:id", authentication, createNewComment);
+commentRouter.post("/:id", authentication,authorization("Create_New_Comment"), createNewComment);
 module.exports = commentRouter;
